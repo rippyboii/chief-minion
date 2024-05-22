460a19739a99319116be02bb113ea5d537be4e55
rippyboii <apeelsubedi@gmail.com>
2024-05-22 19:38:46 +0545

    mistyped token, fixed it again

--- chief.py
+++ chief.py
@@ -183,4 +183,4 @@ async def keep_alive():
     while not bot.is_closed():
         await asyncio.sleep(3600)  # Run task every hour
 
-bot.run(os.getenv("TOKEN"))
+bot.run(os.getenv("DISCORD_BOT_TOKEN"))
